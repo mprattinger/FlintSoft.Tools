c71649b775bffef098e6d5e492b09fbea26b5580
Michael Prattinger <mprattinger@outlook.com>
2022-05-16 09:55:37 +0200

    Adding a constructor to return value model

--- src/FlintSoft.Tools/Models/ReturnModel.cs
+++ src/FlintSoft.Tools/Models/ReturnModel.cs
@@ -5,4 +5,14 @@ public class ReturnModel<T>
     public bool HasException { get; set; }
     public Exception? Exception { get; set; }
     public T? ReturnedValue { get; set; }
+
+    public ReturnModel(T returnedValue, Exception? ex = null)
+    {
+        ReturnedValue = returnedValue;
+        if(ex != null)
+        {
+            Exception = ex;
+            HasException = true;
+        }
+    }
 }
