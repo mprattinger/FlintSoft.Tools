65aa8fa75d5935b355d7f0e42c2e0e04fae5ee21
Michael Prattinger <mprattinger@outlook.com>
2022-05-16 09:36:34 +0200

    Adding a return model

---
+++ src/FlintSoft.Tools/Models/ReturnModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FlintSoft.Tools.Models;
+
+public class ReturnModel<T>
+{
+    public bool HasException { get; set; }
+    public Exception? Exception { get; set; }
+    public T? ReturnedValue { get; set; }
+}
