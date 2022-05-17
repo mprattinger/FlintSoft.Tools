af72024ce8f7edd901cc76deb9acbae263dd54a9
Michael Prattinger <mprattinger@outlook.com>
2022-05-17 15:07:12 +0200

    adding an configuration exception

---
+++ src/FlintSoft.Tools/Exceptions/MissingConfigurationException.cs
@@ -0,0 +1,24 @@
+﻿namespace FlintSoft.Tools.Exceptions;
+
+/// <summary>
+/// Exception thrown when a configuration is missing
+/// </summary>
+public class MissingConfigurationException : Exception
+{
+    public string Section { get; set; } = "";
+
+    public MissingConfigurationException(string section) : base(string.Format("Configuration section '{0}' is missing, configuration couldn't be loaded!", section))
+    {
+        Section = section;
+    }
+
+    public MissingConfigurationException(string section, string errorMessage) : base(errorMessage)
+    {
+        Section = section;
+    }
+
+    public MissingConfigurationException(string section, string errorMessage, Exception innerException) : base(errorMessage, innerException)
+    {
+        Section = section;
+    }
+}
